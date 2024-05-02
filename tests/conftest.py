2eb889808bcdf5102f8dce2954829f2f155941fd
niko-on-telegram <nikoontelegram@gmail.com>
2024-05-02 11:08:18 +0400

    renewed sending spam check sending ban report to chat

---
+++ tests/conftest.py
@@ -0,0 +1,13 @@
+import os
+
+os.environ["BOT_TOKEN"] = ""
+os.environ["WHITELIST"] = "[]"
+os.environ["REPORTS_CHAT_ID"] = "100500"
+os.environ["PERMANENT_BAN"] = "false"
+os.environ["BAN_TIME"] = "35"
+os.environ["COOLDOWN_TIME"] = "604800"
+os.environ["VALIDATOR_URL"] = "http://web-app:7777"
+os.environ["REDIS_HOST"] = "redis"
+os.environ["REDIS_DB"] = "5"
+os.environ["VALIDATOR_USER"] = "123"
+os.environ["VALIDATOR_PASS"] = "qwe"
